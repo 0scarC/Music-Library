090eba90ac16d8f203bc86373d93b65d41531fb3
Oscar <admin@Admins-MacBook-Pro-2.local>
2022-07-18 15:05:04 -0500

    artist and album refactored

--- src/components/AlbumView.js
+++ src/components/AlbumView.js
@@ -1,14 +1,45 @@
+import { render } from '@testing-library/react'
 import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useNavigate } from 'react-router-dom'
 
 function AlbumView() {
     var { id } = useParams()
     var [ albumData, setAlbumData ] = useState([])
+    var navigate = useNavigate()
+
+    useEffect(() => {
+        var API_URL = `http://localhost:4000/song/${id}`
+        var fetchData = async () => {
+            var response = await fetch(API_URL)
+            var resData = await response.json()
+            setAlbumData(resData.results)
+        }
+        fetchData()
+    }, [id])
+
+    var justSongs = albumData.filter(entry => entry.wrapperType === 'track')
+    var renderSongs = justSongs.map((song, i) => {
+        return (
+            <div key={i}>
+                <p>{song.trackName}</p>
+            </div>
+        )
+    })
+
+    var navButtons = () => {
+        return (
+            <div>
+                <button onClick={() => navigate(-1)}>Back</button>
+                |
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
+        )
+    }
 
     return (
         <div>
-            <h2>The album id passed was: {id}</h2>
-            <p>Album Data</p>
+            {navButtons()}
+            {renderSongs}
         </div>
     )
 }
--- src/components/ArtistView.js
+++ src/components/ArtistView.js
@@ -1,14 +1,47 @@
 import { useState, useEffect } from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useParams, Link, useNavigate } from 'react-router-dom'
 
 function ArtistView() {
     var { id } = useParams()
     var [ artistData, setArtistData ] = useState([])
+    var navigate = useNavigate()
+
+    useEffect(() => {
+        var API_URL = `http://localhost:4000/album/${id}`
+        var fetchData = async () => {
+            var response =  await fetch(API_URL)
+            var resData = await response.json()
+            setArtistData(resData.results)
+        }
+        fetchData()
+    }, [id])
     
+    var justAlbums = artistData.filter(entry => entry.collectionType === 'Album')
+    var renderAlbums = justAlbums.map((album, i) => {
+        return (
+            <div key={i}>
+                <Link to={`/album/${album.collectionId}`}>
+                    <p>{album.collectionName}</p>
+                </Link>
+            </div>
+        )
+    })
+
+    var navButtons = () => {
+        return (
+            <div>
+                <button onClick={() => navigate(-1)}>Back</button>
+                |
+                <button onClick={() => navigate('/')}>Home</button>
+            </div>
+        )
+    }
+
     return (
         <div>
-            <h2>The artist id passed was: {id}</h2>
-            <p>Artist Data</p>
+            {navButtons()}
+            {artistData.length > 0 ? <h2>{artistData[0].artistName}</h2> : <h2>Loading...</h2>}
+            {renderAlbums}
         </div>
     )
 }
